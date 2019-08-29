b40148a0f1fa6b269117f489036755290ba160a2
Dan Currie <danjc_15@hotmail.com>
2019-08-28 21:20:51 -0400

    Added Fibonacci Generator
    -made in visual studio with C#
    -prints n fibonacci numbers

---
+++ Fibonacci Generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci_Generator
+{
+    class Program
+    {
+        public static void GenerateFibonacciRecursively(int num, int val1 = 1, int val2 = 1)
+        {
+            if (num > 0)
+            {
+                num--;
+                Console.WriteLine(val1);
+                GenerateFibonacciRecursively(num, val2, val1 + val2);
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Print n Fibonacci Numbers!");
+            Console.WriteLine("Enter number n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            GenerateFibonacciRecursively(n);
+        }
+    }
+}
